8b22c648094724671ca4418260a4ffc53d505ec4
DatVT36 <vuthanhdat181@gmail.com>
2024-11-28 08:29:53 +0700

    feat: add account repository [datvt]

---
+++ FA24_PRN221_3W_G1_KoiFarmShop/KoiFarmShop.Repositories/AccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using KoiFarmShop.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiFarmShop.Repositories
+{
+    public class AccountRepository:GenericRepository<Account>
+    {
+        public AccountRepository()
+        {
+              
+        }
+    }
+}
